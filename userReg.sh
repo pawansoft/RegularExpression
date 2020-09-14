0d1f9975877a35e53c6c95fc29510dbb49d1f5c3
Pawan Kumar <pk.soft29@gmail.com>
2020-09-15 04:23:33 +0530

    Added : Email validation added

--- userReg.sh
+++ userReg.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 expr="^[A-Z][a-z]{3,}$"
+emailExpr="^[a-zA-Z0-9+_.-]+@[a-zA-Z.-]{1,3}+$"
 read -p "Enter your First Name: " name
 read -p "Enter the Last Name: " lastName
+read -p "Enter your email id: " email
 if [[ $name =~ $expr && $lastName =~ $expr ]]
 then
 	echo "both accepted"
 else
 	echo "both Rejected"
+fi
+if [[ $email =~ $emailExpr ]]
+then
+	echo "Email Id accepted"
+else
+	echo "Email Id Rejected"
 fi
