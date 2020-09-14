71f9605fac1fc46568ece37aeeee2d0983fadc5e
Pawan Kumar <pk.soft29@gmail.com>
2020-09-15 04:34:22 +0530

    Added: Mobile number validation added

--- userReg.sh
+++ userReg.sh
@@ -2,9 +2,11 @@
 
 expr="^[A-Z][a-z]{3,}$"
 emailExpr="^[a-zA-Z0-9+_.-]+@[a-zA-Z.-]{1,3}+$"
+mobExpr="^[0-9]{2,2}[ ][0-9]{10,10}"
 read -p "Enter your First Name: " name
 read -p "Enter the Last Name: " lastName
 read -p "Enter your email id: " email
+read -p "Enter Mobile number : " mob
 if [[ $name =~ $expr && $lastName =~ $expr ]]
 then
 	echo "both accepted"
@@ -16,4 +18,10 @@ then
 	echo "Email Id accepted"
 else
 	echo "Email Id Rejected"
+fi
+if [[ $mob =~ $mobExpr ]]
+then
+	echo "Valid Mobile number"
+else
+	echo "Invalid Mobile number"
 fi
